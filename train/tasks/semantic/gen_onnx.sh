205920f38d8ea3a0d92070bac0115369b0fa3d50
Guilherme Nardari <guinardari@usp.br>
2022-07-20 16:08:59 -0300

    small tweaks to helper scripts

--- train/tasks/semantic/gen_onnx.sh
+++ train/tasks/semantic/gen_onnx.sh
@@ -1,8 +1,9 @@
 eval "$(conda shell.bash hook)"
 conda activate seg
 
-CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/ -l ../../../models/quad_bdark/ -m ../../../models/quad_bdark
+# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/ -l ../../../models/quad_bdark/ -m ../../../models/quad_bdark
 # SIMBASIC DARKNET
-# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/simulated_data/ -l ../../../models/sim_quad_bdark/ -m ../../../models/sim_quad_bdark
-
-# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/simulated_data/ -l ../../../models/sim_quad_squeezesegv2/ -m ../../../models/sim_quad_squeezesegv2
+# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/simulated_data/ -l ../../../models/quad_erfnet/ -m ../../../models/quad_erfnet
+CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/ -l ../../../models/arkansas/quad_bdark_knn/ -m ../../../models/arkansas/quad_bdark_knn
+# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/tom_labeled_files/ -l ../../../models/15tom_fast_darknet/ -m ../../../models/15tom_fast_darknet
+# CUDA_VISIBLE_DEVICES="" python3.7 create_onnx.py -d /home/gnardari/Documents/data/ -l ../../../models/quad_simple_erfnet/ -m ../../../models/quad_simple_erfnet
--- train/tasks/semantic/train.sh
+++ train/tasks/semantic/train.sh
@@ -1,6 +1,9 @@
 eval "$(conda shell.bash hook)"
 conda activate seg
 
-python3.7 ./train.py -d /home/gnardari/Documents/data/ -ac ./config/arch/quad/quad_bdarknet.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/quad_bdark/ -p ../../../models/sim_quad_bdark
+python3.7 ./train.py -d /home/gnardari/Documents/data/ -ac ./config/arch/quad/quad_bdarknet.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/arkansas/quad_bdark_knn/ -p ../../../models/sim/sim_quad_bdark
+# python3.7 ./train.py -d /home/gnardari/Documents/data/ -ac ./config/arch/quad/quad_erfnet.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/quad_erfnet/ -p ../../../models/quad_sim_erfnet
+# python3.7 ./train.py -d /home/gnardari/Documents/data/800_data/ -ac ./config/arch/quad/quad_bdarknet_800m.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/800m/quad_bdark/ -p ../../../models/sim/sim_quad_bdark
+# python3.7 ./train.py -d /home/gnardari/Documents/data/800_data/ -ac ./config/arch/quad/quad_erfnet.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/800m/quad_erfnet/ -p ../../../models/sim/quad_sim_erfnet
 # PRETRAINED WITH SIM
 # python3.7 ./train.py -d /home/gnardari/Documents/data/ -ac ./config/arch/quad_squeezesegv2.yaml -dc ./config/labels/quad_binary.yaml -l ../../../models/quad_squeeze/ -p ../../../models/sim_quad_squeeze
